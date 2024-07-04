4d56d0ca028a7f37d7e736f22a028a1c52cc3772
JuanManuelDorrego <138993244+JuanManuelDorrego@users.noreply.github.com>
2024-07-04 18:08:57 -0300

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,9 @@
+
+import { sumar, restar } from './math.js';
+
+const resultadoSuma = sumar(12, 6);
+
+const resultadoResta = restar(15, 5);
+
+console.log(`Suma: ${resultadoSuma}`); 
+console.log(`Resta: ${resultadoResta}`); 
