1d16be872266d903d3e3e8160ca9f9550421ea8a
JuanManuelDorrego <138993244+JuanManuelDorrego@users.noreply.github.com>
2024-07-04 18:21:54 -0300

    Add files via upload

---
+++ tp.js
@@ -0,0 +1,11 @@
+
+export class Persona {
+    constructor(nombre, edad) {
+      this.nombre = nombre;
+      this.edad = edad;
+    }
+  
+    mostrarInformacion() {
+      return `Nombre: ${this.nombre}, Edad: ${this.edad}`;
+    }
+  }
